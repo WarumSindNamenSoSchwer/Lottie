e75acc46e4358a895ba21d361255cf0b2f1a9ad5
WarumSindNamenSoSchwer <muri5988@gmail.com>
2024-02-04 18:42:11 +0100

    init commit

---
+++ Lottie.py
@@ -0,0 +1,60 @@
+#Importieren
+import math
+
+#Operatoren 
+
+'''
++       Plus
++=      Plus und Zuweisung
+-       Minus  
+-=      Minus und Zuweisung
+/       Geteilt
+*       Mal
+=       Zuweisung
+<       Kleiner
+>       Groesser
+<=      Kleinergleich
+>=      Groessergleich
+==      Vergleichsop
+'''
+
+#Variablen
+a = 10
+b = 4
+
+#Plus
+a + b
+
+#Plus und Zuweisung
+a += b #(a = a + b)
+
+#Minus
+a - b
+
+#Minus und Zuweisung
+a -= b #(a = a - b)
+
+#Geteilt
+a / b
+
+#Mal
+a * b
+
+#Zuweisung
+a = b
+
+#Kleiner
+a < b
+
+#Groesser
+a > b
+
+#Kleinergleich
+a <= b
+
+#Groessergleich
+a >= b
+
+#Vergleichsop
+a == b
+
